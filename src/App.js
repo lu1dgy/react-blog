7a1d8c7deb353b7c51e756328dba14164c9115df
alex <lapkesa@gmail.com>
2023-03-04 15:43:02 +0200

    change styles and struct for redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import Profile from './components/Profile/Profile'
+import Dialogs from './components/Dialogs/Dialogs'
+import { Route, Routes } from 'react-router-dom'
 
-const App = () => {
+const App = (props) => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      <div className='app-wrapper-content'>
+        <Routes>
+          <Route
+            path='/dialogs'
+            element={<Dialogs state={props.state.dialogsPage} />}
+          />
+          <Route
+            path='/profile'
+            element={
+              <Profile
+                profilePage={props.state.profilePage}
+                dispatch={props.dispatch}
+              />
+            }
+          />
+        </Routes>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import s from './Navbar.module.css'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink
+          to='/profile'
+          className={(link) => (link.isActive ? s.activeLink : '')}>
+          Profile
+        </NavLink>
+      </div>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink
+          to='/dialogs'
+          className={(link) => (link.isActive ? s.activeLink : '')}>
+          Messages
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <a>News</a>
+      </div>
+      <div className={s.item}>
+        <a>Music</a>
+      </div>
+      <div className={s.item}>
+        <a>Settings</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
